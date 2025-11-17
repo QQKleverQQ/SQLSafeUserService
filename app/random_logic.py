b2182fec565d1f965eeea968b9c87fcabde96817
D.Iatskevich <128312136+QQKleverQQ@users.noreply.github.com>
2025-11-17 14:43:49 +0200

    Add combine_numbers function to sum user input

--- app/random_logic.py
+++ app/random_logic.py
@@ -1,1 +1,8 @@
+import random
 
+def combine_numbers(user_number: int):
+    rand = random.randint(1, 10)
+    total = user_number + rand
+    if total > 10:
+        raise ValueError("Sum exceeds limit 10")
+    return total
